25ddfcc97f3cf72f77fe78a572a6724f0a1979ef
iMitaka <dimitar.georgiev1992@gmail.com>
2018-02-26 12:29:44 +0200

    .

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import Routes from '../routes/routes'
-import 'bootstrap/dist/css/bootstrap.css';
 
 class App extends React.Component {
   render() {
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { render } from 'react-dom';
 import App from './components/App';
 import { BrowserRouter } from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.css';
 
 // Since we are using HtmlWebpackPlugin WITHOUT a template, we should create our own root node in the body element before rendering into it
 let root = document.createElement('div');
@@ -9,6 +10,8 @@ root.id = "root";
 document.body.appendChild(root);
 
 // Now we can render our application into it
-render(<BrowserRouter>
-    <App />
-</BrowserRouter>, document.getElementById('root'));
+render(
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>,
+    document.getElementById('root'));
